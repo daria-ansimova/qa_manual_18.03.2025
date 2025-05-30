ca91e238b38c2643639273a35dca98ae7e064b54
Daria Ansimova <dasha@unix4all.com>
2025-05-30 16:28:37 +0300

    Test file, commit 2

--- qa_manual_18.03.2025.py
+++ qa_manual_18.03.2025.py
@@ -20,3 +20,32 @@ if user_variable_name:
         print("True")
 else:
     print("False")
+
+# task2
+# while True:
+#     user_number_1 = float(input("Enter 1 number: "))
+#     user_number_2 = float(input("Enter 2 number: "))
+#     user_activity = input("Enter math operator (+, -, *, /): ")
+#
+#     if user_activity == "+":
+#         result = user_number_1 + user_number_2
+#     elif user_activity == "-":
+#         result = user_number_1 - user_number_2
+#     elif user_activity == "*":
+#         result = user_number_1 * user_number_2
+#     elif user_activity == "/":
+#         if user_number_2 == 0:
+#             result = 'non-existent'
+#             print("You can't divide by 0")
+#         else:
+#             result = user_number_1 / user_number_2
+#     else:
+#         result = 'non-existent'
+#         print("Error: change math operator")
+#
+#     print(f"Result is {result}")
+#
+#     next_use = input("Would you like to make another calculation? (yes/no): ").strip().lower()
+#     if next_use not in ["yes", "y"]:
+#         print("It was nice to assist you!")
+#         break
